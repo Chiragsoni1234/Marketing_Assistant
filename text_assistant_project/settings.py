f662382646f3ed0ba5bbaa0ab52360427f0b22dd
Chiragsoni1234 <chandrabhushangudheniya@gmail.com>
2025-07-02 19:40:29 +0530

    fix: added render domain to ALLOWED_HOSTS

--- text_assistant_project/settings.py
+++ text_assistant_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-exk#5^grib=n%h^91th%obtimt81ial-xum^l@%6j@mga6%c=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
